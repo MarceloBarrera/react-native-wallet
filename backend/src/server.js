20a759ca9e19bd8855d57c51e9a1c34c456de3bd
Marcelo <darmismarcelo@yahoo.com.ar>
2025-10-05 17:39:56 +0100

    wallet - adding cron job and cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
-
+import job from "./config/job.js";
 dotenv.config();
 
 const app = express();
 app.use(rateLimiter);
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+  job.start();
+}
+
+// specify the origins you want to allow here
+app.use(
+  cors({
+    origin: ["https://your-frontend-domain.com", "http://localhost:8001"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
 const PORT = process.env.PORT || 5001;
+
+app.get("/api/health", (req, res) => {
+  res.status(200).send("API is healthy");
+});
 
 app.use("/api/transactions", transactionsRoute);
 
